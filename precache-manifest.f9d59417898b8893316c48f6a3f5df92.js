93c117a165b4517b68fd88f636430c65c28f686c
Anurag Kumar <anuragak@users.noreply.github.com>
2019-05-05 16:38:32 +0100

    Updates

---
+++ precache-manifest.f9d59417898b8893316c48f6a3f5df92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262ec7a81b13fbad7051160028d369c6",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "0880777925dd34f2bd4e",
+    "url": "/recipe-app/static/css/2.1e66eaca.chunk.css"
+  },
+  {
+    "revision": "980f2d2953e89c039860",
+    "url": "/recipe-app/static/css/main.9d592844.chunk.css"
+  },
+  {
+    "revision": "0880777925dd34f2bd4e",
+    "url": "/recipe-app/static/js/2.9b3ead1c.chunk.js"
+  },
+  {
+    "revision": "980f2d2953e89c039860",
+    "url": "/recipe-app/static/js/main.ae2b6767.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
